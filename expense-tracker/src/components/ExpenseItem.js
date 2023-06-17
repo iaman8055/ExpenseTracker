d01ad889a43fa5b072cf06aaee92098c7184551f
iaman8055 <iamanchauhan93@gmail.com>
2023-06-17 21:21:53 +0530

    minor changes

--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
+import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css';
-function ExpenseItem(){
+function ExpenseItem(props){
     return (
         <div className="expense-item">
-            <div>March 28th 2023</div>
+           <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
+                <h2>{props.title}</h2>
                 </div>
-            <div className="expense-item__price">$297.97</div>
+            <div className="expense-item__price">${props.amount}</div>
         </div>
     );
 }
