901eb4c44b7eb1260e0833fe1f62772892941dd9
iaman8055 <iamanchauhan93@gmail.com>
2023-06-23 01:59:39 +0530

    Finished

--- expense-tracker/src/components/Expenses/ExpenseItem.js
+++ expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -3,19 +3,17 @@ import ExpenseDate from './ExpenseDate'
 import Card from '../UI/card';
 import './ExpenseItem.css';      
 function ExpenseItem(props){
-    const[title,setTitle]=useState(props.title)
-    const clickhandler=()=>{
-        setTitle('!updated')
-    }
+    
     return (
+        <li>
         <Card className="expense-item">
            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 </div>
             <div className="expense-item__price">${props.amount}</div>
-            <button onClick={clickhandler}>Change Title</button>
         </Card>
+        </li>
     );
 }
 export default ExpenseItem;
